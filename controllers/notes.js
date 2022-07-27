e7b95d4088c86998f687e968a6171f5851b3e559
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-07-26 20:43:23 -0700

    post attempt

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
 const fs = require('fs');
+const app = require('.');
 
 notes.get('/', (req, res) => {
     console.info(`${req.method} request recieved for Notes`);
@@ -13,5 +14,44 @@ notes.get('/', (req, res) => {
         }
     });
 });
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        }
+    fs.readFile('./db/notes/json', 'utf8', (err, data) => {
+        if (err){
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+
+            parsedNotes.push(newNote);
+
+            fs.writeFile(
+                './db/notes.json',
+                JSON.stringify(parsedNotes, null, 2),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully updated notes!')
+            );
+            }
+        });
+
+    const response = {
+        status: 'Success!',
+        body: newNote,
+    };
+    console.log(response);
+    res.json(response);
+} else {
+    res.status(500).json('Error in posting note');
+}
+});
+
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -18,19 +18,19 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+// GET Route for feedback page
+app.get('./notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+
