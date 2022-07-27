d625c5097f47878f042ca3d44eaee0c55c626e11
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-07-27 01:42:31 -0700

    delete works

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const { json } = require('express');
 
 const PORT = process.env.PORT || 3001;
 
@@ -29,50 +30,75 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-// app.get('/api/notes/:note_id', (req, res) => {
-//   console.info(`${req.method} request recieved for Notes`);
-//   const noteId = req.params.note_id;
-//   res.sendFile(path.join(__dirname, './db/db.json'))
-//     .then((data) => JSON.parse(data))
-//     .then((json) => {
-//       const result = json.filter(() => note.note_id === noteId);
-//       return result.length > 0
-//         ? res.json(result)
-//         : res.json('No note with that ID');
-//     });
-// });
+app.get('/api/notes/:id', (req, res) => {
+  console.info(`${req.method} request recieved for Notes`);
+  const noteId = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data)
+      const result = notes.filter((note) => note.id === noteId);
+      return result.length > 1
+        ? res.json(result)
+        : res.json('No note with that ID');
+    }});
+});
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved for Notes`);
-    console.log(req.body)
-    const { title, text } = req.body
-    const newNote = {
-      title: title,
-      text: text,
-      note_id: uuidv4(),
+  console.info(`${req.method} request recieved for Notes`);
+  console.log(req.body)
+  const { title, text } = req.body
+  const newNote = {
+    title: title,
+    text: text,
+    id: uuidv4(),
+  }
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+      console.log(notes);
+      fs.writeFile('./db/db.json', JSON.stringify(notes),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
     }
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const notes = JSON.parse(data);
-        notes.push(newNote);
-        console.log(notes);
-        fs.writeFile('./db/db.json', JSON.stringify(notes),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
-        );
-      }
-    });
-    const response = {
-      status: 'Success!',
-      body: newNote,
-    };
-    console.log(response);
-    res.json(response);
   });
+  const response = {
+    status: 'Success!',
+    body: newNote,
+  };
+  console.log(response);
+  res.json(response);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  console.info(`${req.method} request recieved for Notes`);
+  const noteId = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data);
+      const newNotes = notes.filter(note => note.id !== noteId)
+      fs.writeFile('./db/db.json', JSON.stringify(newNotes), function(err, data) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(data)
+          res.json(`Note ${noteId} has been delted`) 
+        } 
+      })
+    }
+        
+  })
+ 
+});
 
 
 
